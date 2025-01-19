b3f5ae848ec4d962bfe3e37605a48f789b08bac6
Alisson Araújo Mariano <157244142+Alissonaraujodev@users.noreply.github.com>
2025-01-19 12:28:25 -0300

    logica em JavaScript
    finalizando o o jogo do número secreto, e novos exercícios de fixação.

--- logica-js-projeto_inicial/desafio-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/desafio-js-projeto_inicial/app.js
@@ -1,42 +1,42 @@
+/*// Desafio 1 
 
-/* Desafio 1 */
-
-/*1*/
+//1
 alert("Boas vindas ao nosso site!");
 
-/*2*/
+//2
 let nome = "Lua";
 
-/*3*/
+//3
 let idade = 25;
 
-/*4*/
+//4
 let numeroDeVendas = 50;
 
-/*5*/
+//5
 let saldoDisponivel = 1000;
 
-/*6*/
+//6
 alert("Erro! Preencha todos os campos");
 
-/*7*/
+//7
 let mensageDeErro = "Erro! Preencha todos os campos";
 alert(mensageDeErro);
 
-/*8*/
+//8
 let nome1 = prompt("Digite seu nome:");
 
-/*9*/
+//9
 let idade1 = prompt("Digite sua idade:");
 
-/*10*/
+//10
 if(idade1 >= 18){
     alert("Pode tirar a habilitação!");
 }
+*/
 
-/* Desafio 2 */
-
-/*1*/
+/*//Desafio 2
+ 
+//1
 let diaSemana = prompt("Que dia da semana é hoje? ");
 
 if(diaSemana == "sabado" || diaSemana == "domingo"){
@@ -45,7 +45,7 @@ if(diaSemana == "sabado" || diaSemana == "domingo"){
     alert("Boa semana");
 }
 
-/*2*/
+//2
 let numero = prompt("digite um número");
 
 if(numero > 0){
@@ -54,7 +54,7 @@ if(numero > 0){
     alert("Número negativo")
 }
 
-/*3*/
+//3
 let pontuacao = prompt("Digite sua pontuação")
 
 if(pontuacao >= 100){
@@ -63,10 +63,123 @@ if(pontuacao >= 100){
     alert("Tente novamente para ganhar")
 }
 
-/*4*/
+//4
 let saldo = 500
 alert(`Seu saldo é de R$ ${saldo}.`)
 
-/*5*/
+//5
 let nome2 = prompt("Digite seu nome:")
 alert(`Seja bem vindo ${nome2}!`)
+*/
+
+/*//Desafio 3
+
+//1
+let contadorCrescente = 1;
+while(contadorCrescente <= 10){
+    console.log(contadorCrescente);
+    contadorCrescente++;
+}
+
+//2
+let contadorDecrescente = 10;
+while(contadorDecrescente >= 0){
+    console.log(contadorDecrescente);
+    contadorDecrescente--;
+}
+
+//3
+let contagemRegressiva = prompt('Digite um número:');
+
+while(contagemRegressiva >= 0){
+    console.log(contagemRegressiva);
+    contagemRegressiva--;
+}
+
+//4
+
+let numero = prompt('Digite um número:');
+
+contagemProgressiva = 0;
+while(numero >= contagemProgressiva){
+    console.log(contagemProgressiva);
+    contagemProgressiva++;
+}
+*/
+
+/*//Desafio 4
+
+//1
+console.log("Seja bem vindo!");
+
+//2
+let nome = "Alisson"
+console.log(`Olá ${nome}.`);
+
+//3
+let nome1 = "Alisson"
+alert(`Olá ${nome1}.`);
+
+//4
+let linguaguemProgramacaoMaisGosta = prompt("Qual a linguagem de programação você mais gosta?");
+console.log(linguaguemProgramacaoMaisGosta);
+
+//5
+let valor1 = 10;
+let valor2 = 5;
+
+resultadoSoma = valor1 + valor2;
+console.log(`A soma de ${valor1} e ${valor2} é igual a ${resultadoSoma}`);
+
+//6
+let valor3 = 10;
+let valor4 = 5;
+
+let resultadoSubtracao = valor3 - valor4;
+console.log(`A diferença entre ${valor3} e ${valor4} é igual a ${resultadoSubtracao}`);
+
+//7
+let idade = prompt("Digite sua idade:")
+if(idade >= 18){
+    console.log("Você é maior de idade.");
+}else{
+    console.log("Você é menor de idade.");
+}
+
+//8
+let numero = prompt("Digite um número:")
+if(numero > 0){
+    console.log(`O número ${numero} é positivo.`)
+}else if(numero < 0){
+    console.log(`O número ${numero} é negativo`)
+}else{
+    console.log(`O número ${numero} é neutro.`)
+}
+
+//9
+let contador = 1;
+while(contador <= 10){
+    console.log(contador)
+    contador++
+}
+
+//10
+let nota = 6;
+if(nota >= 7){
+    console.log("Parabéns, você foi aprovado")
+}else{
+    console.log("Infelizmente você foi reprovado")
+}
+
+//11
+let numeroAleatorio = Math.random()
+console.log(numeroAleatorio)
+
+//12
+let numeroAleatorio1 = parseInt(Math.random() *10 + 1)
+console.log(numeroAleatorio1)
+
+//13
+let numeroAleatorio2 = parseInt(Math.random() *1000 + 1)
+console.log(numeroAleatorio2)
+*/
--- logica-js-projeto_inicial/jogoNumeroSecreto/app.js
+++ logica-js-projeto_inicial/jogoNumeroSecreto/app.js
@@ -1,14 +1,28 @@
-/*alert ("Boas vindas ao jogo do número secreto.");
-let numeroSecreto = 5
+alert ("Boas vindas ao jogo do número secreto.");
+numeroMax = 5000;
+let numeroSecreto = parseInt(Math.random() * numeroMax + 1);
 
-let chute = prompt("Escolha um número entre 1 e 10:");
+let chute;
+let tentativas = 1;
 
-if(numeroSecreto == chute){
-    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}`);
-}else{
-    alert("Você errou :(")
-}*/
 
+while(chute != numeroSecreto){
+   chute = prompt(`Escolha um número entre 1 e ${numeroMax}`);
+   
+   if(chute == numeroSecreto){
+    break;
+    }else{
+        if(chute > numeroSecreto){
+            alert(`O número secreto é menor que ${chute}`);
+        }else{
+            alert(`O número secreto é maior que ${chute}`);
+        }
+        tentativas++;
+    }
+}
+//operador ternario "simplificação do if else"
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa'
+alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}!`);
 
 
 
