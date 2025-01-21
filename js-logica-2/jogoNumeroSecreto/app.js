922876823344be51f102d61b71af56ff8324cd65
Alisson Araújo Mariano <157244142+Alissonaraujodev@users.noreply.github.com>
2025-01-20 22:10:34 -0300

    logica com JavaScript
    manipulando HTML com JavaScript

---
+++ js-logica-2/jogoNumeroSecreto/app.js
@@ -0,0 +1,11 @@
+const { NotFoundBoundary } = require("next/dist/client/components/not-found-boundary");
+
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Jogo do número secreto';
+
+let paragrafo = document.querySelector('p');
+paragrafo.innerHTML = 'Escolha um número entre 1 e 10:';
+
+function verificarChute(){
+    
+}
