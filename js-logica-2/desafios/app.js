47d50da24143d96203948de39071b3d28ad046be
Alisson Araújo Mariano <157244142+Alissonaraujodev@users.noreply.github.com>
2025-01-24 20:20:32 -0300

    Tipos de dados e desafio
    Fazendo a comparação do chute e do numero gerado pela função gerarNumeroAleatorio(), e retornando o valor booleano no console, resolvi os desafios propostos para fixação do conteúdo.

--- js-logica-2/desafios/app.js
+++ js-logica-2/desafios/app.js
@@ -1,5 +1,5 @@
 //Desafio 1
-
+/*
 //2
 let titulo = document.querySelector('h1');
 titulo.innerHTML = 'Hora do desafio';
@@ -28,3 +28,57 @@ function soma(){
     let soma = Number(numero1) + Number(numero2);
     alert(`A soma entre ${numero1} e ${numero2} é igual a ${soma} `)
 }
+*/
+
+//Desafio 2
+/*
+//1
+function olaMundo(){
+    console.log('Olá Mundo!');
+}
+
+olaMundo();
+
+//2
+function cumprimento(name){
+    console.log(`Olá ${name}.`);
+}
+
+cumprimento('Alisson');
+
+//3
+function dobroX(number){
+    return number*2;
+}
+
+console.log(dobroX(2));
+
+//4
+function media(num1, num2, num3){
+    return (num1 + num2 + num3) / 3;
+}
+
+console.log(media(1, 2, 3));
+
+//5
+function maior(num1, num2){
+    if(num1 > num2){
+        return num1;
+    }else{
+        return num2;
+    }
+}
+
+console.log(maior(6, 10));
+
+//6
+function multiplicandoXPorX(num1){
+    return num1 * num1;
+}
+
+console.log(multiplicandoXPorX(5))
+*/
+
+//Desafio 3
+
+
