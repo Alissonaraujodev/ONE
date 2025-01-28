2ac4bdd28a64c57e08140cd3886a3897f9075169
Alisson Araújo Mariano <157244142+Alissonaraujodev@users.noreply.github.com>
2025-01-27 21:19:28 -0300

    Listas e desafios
    adicionando listas(arrays), para o programa não sortear o mesmo número 2 vezes antes de sortear todos os números possíveis, resolução dos desafios para fixação do conteúdo.

--- js-logica-2/desafios/app.js
+++ js-logica-2/desafios/app.js
@@ -141,3 +141,26 @@ tabuada(5);
 */
 
 //Desafio 4 
+/*
+//1
+let listaGeneriaca = [];
+
+//2
+let linguagensDeProgramacao = ['JavaScript', 'C', 'C++', 'Kotlin', 'Python'];
+
+//3
+linguagensDeProgramacao.push('Java', 'Ruby', 'GoLang');
+
+//4
+let listaNomes = ['Alisson', 'Adrielly', 'Aline'];
+console. log(listaNomes[0]);
+
+//5
+console.log(listaNomes[1]);
+
+//6
+console.log(listaNomes[listaNomes.length -1]);
+*/
+
+//Desafio 5
+//1
--- js-logica-2/jogoNumeroSecreto/app.js
+++ js-logica-2/jogoNumeroSecreto/app.js
@@ -1,4 +1,5 @@
-//const { NotFoundBoundary } = require("next/dist/client/components/not-found-boundary");
+let listaDeNumerosSorteados = [];
+let numeroLimite = 10;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -6,6 +7,7 @@ function exibirTextoNaTela(tag, texto){
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
 }
+
 function exibirMensagemInicial(){
     exibirTextoNaTela('h1', 'Jogo do número secreto');
     exibirTextoNaTela('p', 'Escolha um número entre 1 e 10:');
@@ -15,7 +17,7 @@ exibirMensagemInicial();
 
 function verificarChute(){
     let chute = document.querySelector('input').value;
-   if(chute == numeroSecreto){
+    if(chute == numeroSecreto){
     exibirTextoNaTela('h1', 'Acertou!');
     let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa'; 
     let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`
@@ -33,7 +35,18 @@ function verificarChute(){
 }
 
 function gerarNumeroAleatorio(){
-   return parseInt(Math.random() * 10 + 1);
+   let numeroEscolhido =  parseInt(Math.random() * numeroLimite + 1);
+   let quantidadeDeElementosNaLista = listaDeNumerosSorteados.length;
+
+   if(quantidadeDeElementosNaLista == numeroLimite){
+    listaDeNumerosSorteados = [];
+   }
+   if(listaDeNumerosSorteados.includes(numeroEscolhido)){
+    return gerarNumeroAleatorio();
+   }else{
+    listaDeNumerosSorteados.push(numeroEscolhido);
+    return numeroEscolhido;
+   }
 }
 
 function limparCampo() {
